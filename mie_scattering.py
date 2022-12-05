09859436d9da26d33f824ffd4414b4e59529e5ba
hecatehonryuu <95301252+hecatehonryuu@users.noreply.github.com>
2022-12-06 02:14:14 +0800

    Fixed simulation laser cutoff

--- mie_scattering.py
+++ mie_scattering.py
@@ -7,7 +7,8 @@ import meep as mp
 wvl = [0.35, 0.4, 0.45, 0.5, 0.6]
 # frq = 1/wvl 
 fnumber = 4.0
-t_width = 4.5
+t_width = 4.5/np.sqrt(4*np.log(2))
+t_cutoff = np.sqrt(4*np.log(2))*2.0
 # s_width = 2.0 * wvl * fnumber / np.pi
 
 #set up box
@@ -24,7 +25,7 @@ for w in wvl:
     # is_integrated=True necessary for any planewave source extending into PML
     sources = [
         mp.GaussianBeamSource(
-            mp.GaussianSource(wavelength = w, width = t_width,cutoff = t_width * 2,is_integrated = True),
+            mp.GaussianSource(wavelength = w, width = t_width,cutoff = t_cutoff,is_integrated = True),
             center = mp.Vector3(-dair/2, 0), 
             size = mp.Vector3(0, s), #line of y = -dair/2 (vert line through entire box)
             beam_x0 = mp.Vector3(dair/2, 0), #Focus at center of box
@@ -55,11 +56,11 @@ for w in wvl:
     )
 
     #Run Simulation and obtain data
-    sim.use_output_directory(f"Test2_{w}") #output all to default directory mie_scattering-out/
+    sim.use_output_directory(f"Test3_{w}") #output all to default directory mie_scattering-out/
     sim.run(
         mp.at_beginning(mp.output_epsilon),
         # mp.to_appended(f"efield_z_{w}", mp.at_every(1, mp.output_efield_z)),
         # mp.at_every(1, mp.output_png(mp.Ez, "-Zc /home/maikuhl/.conda/envs/mp/share/h5utils/colormaps/dkbluered -C $EPS")), #Using h5topng --help to find path due to problem with h5utils that is compatible with meep
-        mp.at_every(1, mp.output_png(mp.Ez, "-Zc /home/draco/miniconda3/envs/mp/share/h5utils/colormaps/dkblueredcustom -C $EPS")),
-        until=60)
+        mp.at_every(1, mp.output_png(mp.Ez, "-Zc /home/draco/miniconda3/envs/mp/share/h5utils/colormaps/dkblueredblack -C $EPS")),
+        until=40)
 
